0ac087df8167566fbb816467f4a72bc20fa61a34
milymoreno <milydemendoza@gmail.com>
2024-07-14 13:48:41 -0500

    Creando Logs

--- src/main/java/com/flypass/financiera/controller/ProductoController.java
+++ src/main/java/com/flypass/financiera/controller/ProductoController.java
@@ -16,39 +16,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.flypass.financiera.model.Producto;
 import com.flypass.financiera.service.ProductoService;
 
+//Para los logs
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductoController.class);
 
     @Autowired
     private ProductoService productoService;
 
     @PostMapping
     public ResponseEntity<Producto> crearProducto(@RequestBody Producto producto) {
+        logger.info("Se ha Creado un Producto con el metodo POST al endpoint /productos");
         Producto nuevoProducto = productoService.crearProducto(producto);
         return ResponseEntity.ok(nuevoProducto);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Producto> actualizarProducto(@PathVariable Long id, @RequestBody Producto productoDetalles) {
+        logger.info("Se ha Actualizado un Producto con el metodo PUT al endpoint /productos/{id}");
         Producto productoActualizado = productoService.actualizarProducto(id, productoDetalles);
         return ResponseEntity.ok(productoActualizado);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarProducto(@PathVariable Long id) {
+        logger.info("Se ha Eliminado un Producto con el metodo DELETE al endpoint /productos/{id}");
         productoService.eliminarProducto(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable Long id) {
+        logger.info("Se ha Listado un Producto con el metodo GET al endpoint /productos/{id}");
         Producto producto = productoService.obtenerProductoPorId(id);
         return ResponseEntity.ok(producto);
     }
 
     @GetMapping
     public ResponseEntity<List<Producto>> obtenerTodosLosProductos() {
+        logger.info("Se han Listado todos los Productos con el metodo GET al endpoint /productos");
         List<Producto> productos = productoService.obtenerTodosLosProductos();
         return ResponseEntity.ok(productos);
     }
--- src/main/java/com/flypass/financiera/controller/TransaccionController.java
+++ src/main/java/com/flypass/financiera/controller/TransaccionController.java
@@ -13,21 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.flypass.financiera.model.Transaccion;
 import com.flypass.financiera.service.TransaccionService;
 
+//Para los logs
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/transacciones")
 public class TransaccionController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransaccionController.class);
 
     @Autowired
     private TransaccionService transaccionService;
 
     @PostMapping
     public ResponseEntity<Transaccion> crearTransaccion(@RequestBody Transaccion transaccion) {
+        logger.info("Se ha Creado una Transaccion con el metodo POST al endpoint /transacciones");
         Transaccion nuevaTransaccion = transaccionService.crearTransaccion(transaccion);
         return ResponseEntity.ok(nuevaTransaccion);
     }
 
     @GetMapping
     public ResponseEntity<List<Transaccion>> obtenerTodasLasTransacciones() {
+        logger.info("Se han Listado Todas las Transacciones con el metodo GET al endpoint /transacciones");
         List<Transaccion> transacciones = transaccionService.obtenerTodasLasTransacciones();
         return ResponseEntity.ok(transacciones);
     }
