73ec3ab91c5fcf92545798e4242b93a953715639
milymoreno <milydemendoza@gmail.com>
2024-07-19 17:40:10 -0500

    Organizando este repo Desordenado

--- src/main/java/com/flypass/financiera/repository/TipoProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flypass.financiera.repository;
-
-import com.flypass.financiera.model.TipoProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoProductoRepository extends JpaRepository<TipoProducto, Long> {
-}
--- src/main/java/com/flypass/financiera/repository/TransaccionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.flypass.financiera.repository;
-
-import com.flypass.financiera.model.Transaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
-}
-
